911bc4479ce3724bfd377d67743885f49dad2a22
zer0 <razviy69@gmail.com>
2023-04-20 18:25:59 +0200

    Default

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./pages/Home/HomePage";
+import Signup from "./pages/Signup/Signup";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/*" element={<HomePage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./HomePage.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  );
+};
+
+export default HomePage;
