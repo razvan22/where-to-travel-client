bcb403d5afb8b9be76cb8f588d602bf6e902c77e
zer0 <razviy69@gmail.com>
2023-04-27 18:48:12 +0200

    Add react query for Home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
-import "./HomePage.css";
-import axios from "axios";
-import { DestinationResponse } from "../../shared/types/location/DestinationResponse";
-import Destination from "../../components/destination/Destination";
+import { useQuery } from "@tanstack/react-query";
 import { getAllDestinations } from "../../hooks/data/destination";
+import Destination from "../../components/destination/Destination";
+import { DestinationResponse } from "../../shared/types/location/DestinationResponse";
+import "./HomePage.css";
+import { Skeleton } from "@mui/material";
+import LoadingLogo from "../../shared/loading-logo/LoadingLogo";
 
 const HomePage: React.FC = () => {
-  const [destinations, setDestinations] = useState<DestinationResponse[]>([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await getAllDestinations();
-      setDestinations(response.data);
-    };
-    getData();
-  }, []);
+  const { isLoading, data } = useQuery(
+    ["destinations-all"],
+    getAllDestinations
+  );
 
   return (
     <section className="home">
-      {destinations.map((destination: DestinationResponse) => (
-        <Destination destination={destination} key={destination.id} />
-      ))}
+      {isLoading ? (
+        <LoadingLogo />
+      ) : (
+        data?.data.map((destination: DestinationResponse) => (
+          <Destination destination={destination} key={destination.id} />
+        ))
+      )}
     </section>
   );
 };
---
+++ src/shared/loading-logo/LoadingLogo.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@mui/material";
+
+const LoadingLogo = () => {
+  return (
+    <>
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+      <Skeleton variant="rectangular" width="25%" height={118} />
+      <Skeleton width="25%" />
+    </>
+  );
+};
+
+export default LoadingLogo;
